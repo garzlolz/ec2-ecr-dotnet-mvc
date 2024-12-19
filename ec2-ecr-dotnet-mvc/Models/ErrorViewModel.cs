a06a568ebb5994c9c24fa8d651fc22959cc29370
garzlolz <oscar861213@gmail.com>
2024-12-19 15:06:11 +0800

    加入專案檔案。

---
+++ ec2-ecr-dotnet-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ec2_ecr_dotnet_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
